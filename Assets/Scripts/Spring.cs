c0ec3801a3042b6e49b4c6d68ce5498184a1aa8a
George Miranda <george92miranda@gmail.com>
2022-02-27 22:22:02 -0800

    update springs

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -7,29 +7,42 @@ public class Spring : MonoBehaviour
     [SerializeField] private AudioClip bounceAudioClip;
     [SerializeField] private int bounceAudioClipVolume = 100;
     [SerializeField] private float bouncePower = 20;
+    private bool isLoaded = true;
+    private float carryDistanceCheck = 0.1f;
+    private float springCooldown = 0.5f;
+    private Rigidbody2D rb;
+    RaycastHit2D[] hits;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (isLoaded)
+        {
+            hits = new RaycastHit2D[16];
+            int hitCount = rb.Cast(Vector2.up, hits, carryDistanceCheck);
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (hits[i].collider.gameObject == Player.Instance.gameObject)
+                {
+                    Animator animator = gameObject.GetComponent<Animator>();
+                    animator.SetTrigger("Bounce");
+                    AudioPlayer.Instance.PlaySFX(bounceAudioClip, bounceAudioClipVolume);
+                    Player.Instance.Bounce(bouncePower);
+                    isLoaded = false;
+                    Invoke("LoadSpring", springCooldown);
+                }
+            }
+        }
     }
 
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    void LoadSpring()
     {
-        if (collision.gameObject == Player.Instance.gameObject)
-        {
-            Animator animator = gameObject.GetComponent<Animator>();
-            animator.SetTrigger("Bounce");
-            AudioPlayer.Instance.PlaySFX(bounceAudioClip, bounceAudioClipVolume);
-            Player.Instance.Bounce(bouncePower);
-        }
+        isLoaded = true;
     }
 }
