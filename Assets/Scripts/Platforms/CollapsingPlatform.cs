3e80d209a160be9311cbeaabaa8a6c42b23511f8
George Miranda <george92miranda@gmail.com>
2022-06-05 14:03:27 -0700

    resetting collaping platforms

--- Assets/Scripts/Platforms/CollapsingPlatform.cs
+++ Assets/Scripts/Platforms/CollapsingPlatform.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Tilemaps;
 
 public class CollapsingPlatform : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class CollapsingPlatform : MonoBehaviour
     [SerializeField] PlayableDirector director;
     [SerializeField] private float dropDelay = 0.75f;
     [SerializeField] private float returnDelay = 3f;
+    [SerializeField] private DeathManagerScriptableObject deathManager;
+    private Tilemap tilemap;
+    private TilemapCollider2D collider;
 
     [Header("Sound Config")]
     [SerializeField] private AudioClip soundClip;
@@ -21,12 +25,27 @@ public class CollapsingPlatform : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         transform.position = Vector3.zero;
+
+        if (deathManager)
+        {
+            deathManager.deathEvent.AddListener(ResetPosition);
+            tilemap = GetComponent<Tilemap>();
+            collider = GetComponent<TilemapCollider2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnDisable()
+    {
+        if (deathManager)
+        {
+            deathManager.deathEvent.RemoveListener(ResetPosition);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -51,5 +70,13 @@ public class CollapsingPlatform : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         isFalling = false;
         yield return null;
+    }
+
+    private void ResetPosition(int deathCount) {
+        director.Stop();
+        collider.enabled = true;
+        tilemap.color = new Color(1, 1, 1, 1);
+        rb.position = Vector2.zero;
+        isFalling = false;
     }
 }
